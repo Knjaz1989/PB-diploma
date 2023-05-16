1d3f19f59da6f51de8c43c29df07456c247df6f8
Igor Sverchkov <knjaz1989@gmail.com>
2023-05-16 21:30:18 +0300

    game works

--- logic.js
+++ logic.js
@@ -1,2 +1,55 @@
 let players = ['x', 'o'];
-let activePlayer = 0;
+let activePlayer = players[0];
+let boardSize = 3;
+let board;
+
+
+function createNeaBoard(size) {
+    let newArray = new Array;
+    for (let index = 0; index < size; index++) {
+        let innerArray = new Array;
+        for (let index = 0; index < size; index++) {
+            innerArray.push('');
+        };
+        newArray.push(innerArray);
+    };
+    return newArray;
+}
+
+
+function startGame() {
+    board = createNeaBoard(boardSize);
+    renderBoard(board);
+}
+
+
+function click(row, column) {
+    board[row][column] = activePlayer 
+    renderBoard(board);
+    if (checkBoard(board)) {
+        showWinner(activePlayer);
+    };
+    if (activePlayer === 'x') {
+        activePlayer = players[1];
+    } else {
+        activePlayer = players[0]
+    }
+}
+
+
+function checkBoard(board) {
+    for (let indexRow = 0; indexRow < board.length; indexRow++) {
+        if (board[indexRow].filter(x => x === activePlayer).length === board.length) {
+            return true;
+        }
+        let columnArray = [];
+        let indexColumn = indexRow;
+        for (let indexRow = 0; indexRow < board.length; indexRow++) {
+            columnArray.push(board[indexRow][indexColumn]);          
+        }
+        if (columnArray.filter(x => x === activePlayer).length === board.length) {
+            return true;
+        }
+    };
+    return false;
+}
